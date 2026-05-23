bdc585f2812522c1259848ca50fd66f83e6a6be1
Washi2358 <washingtonaparicio@gmail.com>
2026-05-23 02:25:57 -0500

    Setup SSH keys and authorized_keys for root
    Create the .ssh directory for root and add public key to authorized_keys.

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+# Generate SSH host keys if they don't exist
 if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
     ssh-keygen -A
 fi
 
-/usr/sbin/sshd -D &
+# Create authorized_keys for root
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
 
-wait
+# Add public key
+cat > /root/.ssh/authorized_keys << 'PUBKEY'
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCh1/HrHTISlpCEgg8f7cBLjv2sUg4hcNtkNtKF8WH/qz9k7ji066d/sjKh2tZhzgFgB1eSAg6z3MFa5vqpk8FZTPIeLzsWm3S9JmsYMn4mWPLx0dig14j1q6lsqmbFgrCCRm/CKgyKlV/XMmnLhFMPZz1IRNPMZCUPN7wGPGnobLvbBi1axp6VZPGltuH4GaokLWLNUReyvi8SUirIzwqorUmmhJqW4B7EpVruP4Zqr7bMTbCyJD5LvDRkOlkirggB00/Pipp9g9DzCeEOIyELgfUMpl+1TCNacovIuvGiRRf/jejZhhbKX173AA0Z+Lgr/nzn7TRTxiS/7kQenWKfpg3cTcH2w33cTwuqqaHcglGnjWL3Hj
