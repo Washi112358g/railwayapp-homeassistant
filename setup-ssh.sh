7bb02fb16643d386d918d7ba862f27952e194416
Washi2358 <washingtonaparicio@gmail.com>
2026-05-23 02:42:01 -0500

    Add ED25519 public key to authorized_keys

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -10,6 +10,15 @@ fi
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
-# Add public key
+# Add public key (ED25519)
 cat > /root/.ssh/authorized_keys << 'PUBKEY'
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCh1/HrHTISlpCEgg8f7cBLjv2sUg4hcNtkNtKF8WH/qz9k7ji066d/sjKh2tZhzgFgB1eSAg6z3MFa5vqpk8FZTPIeLzsWm3S9JmsYMn4mWPLx0dig14j1q6lsqmbFgrCCRm/CKgyKlV/XMmnLhFMPZz1IRNPMZCUPN7wGPGnobLvbBi1axp6VZPGltuH4GaokLWLNUReyvi8SUirIzwqorUmmhJqW4B7EpVruP4Zqr7bMTbCyJD5LvDRkOlkirggB00/Pipp9g9DzCeEOIyELgfUMpl+1TCNacovIuvGiRRf/jejZhhbKX173AA0Z+Lgr/nzn7TRTxiS/7kQenWKfpg3cTcH2w33cTwuqqaHcglGnjWL3Hj
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBcRlPteDbLM9j6uoBv8FTtqqzor9ajooisyQmPtpldc home-assistant-ssh
+PUBKEY
+
+chmod 600 /root/.ssh/authorized_keys
+
+# Start SSH server in background
+/usr/sbin/sshd -D &
+
+# Keep the process running
+wait
